57dc3c3297e7f65fdd638081f8aa0bb877c40890
ManJish <61845899+manzis@users.noreply.github.com>
2024-10-14 20:23:45 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Manjish Upadhaya</title>
+    <link href="https://fonts.gstatic.com/" rel="preconnect" crossorigin="">
+    <link rel="stylesheet" href="./header/navbar.css" />
+    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="./footer/footer.css" />
+  </head> 
+  <body>
+    <div class="main-container">
+<?php include "./header/navbar.php" ?>
+      <div class="frame-24">
+        <div class="frame-25">
+          <div class="frame-26">
+            <div class="frame-27">
+              <div class="frame-28">
+                <div class="manjish"></div>
+                <div class="frame-29">
+                  <span class="span-manjish-upadhaya">Manjish Upadhaya</span><span class="hi">✋</span>
+                </div>
+              </div>
+              <div class="frame-2a">
+                <span class="span-building-digital">Building digital</span
+                ><span class="span-products-brands-experience"
+                  >products, brands, and<br />experience.</span
+                >
+              </div>
+            </div>
+            <button class="button-container">
+              <div class="link-2b">
+                <div class="container-2c">
+                  <div class="container-2d">
+                    <span class="span-latest-works">Latest works</span>
+                  </div>
+                </div>
+                <div class="container-2e">
+                  <div class="svg"><div class="svg-"></div></div>
+                </div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="container-clients">
+      <div class="container-2f">
+        <div class="frame-30">
+          <div class="horizontal-divider"></div>
+          <button class="background-shadow">
+            <div class="border-31"></div>
+            <div class="container-32">
+              <div class="container-33">
+                <span class="span-clients">Clients</span>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div class="frame-34">
+          <div class="frame-35" id="scroll-container">
+            <div class="frame-36"><div class="aribt-redesigned"></div></div>
+            <div class="frame-37"><div class="lopho-abroad"></div></div>
+            <div class="frame-38"><div class="emblem-of-nepal"></div></div>
+            <div class="frame-39"><div class="logo-ismt"></div></div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="container-services">
+      <div class="container-3a">
+        <div class="frame-3b">
+          <span class="collaborate-with-brands"
+            >Collaborate with brands and agencies to create impactful
+            results</span
+          >
+        </div>
+        <div class="frame-3c">
+          <div class="horizontal-divider-3d"></div>
+          <button class="background-shadow-3e">
+            <div class="border-3f"></div>
+            <div class="container-40">
+              <div class="container-41">
+                <span class="my-services">My Services</span>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div class="frame-42">
+          <div class="frame-43">
+            <div class="frame-44">
+              <div class="container-45">
+                <div class="container-46">
+                  <div class="svg-47"><div class="svg-48"></div></div>
+                </div>
+                <div class="container-49">
+                  <div class="container-4a">
+                    <div class="container-4b">
+                      <span class="ux-ui">UX & UI</span>
+                    </div>
+                  </div>
+                  <div class="container-4c">
+                    <div class="container-4d">
+                      <span class="designing-interfaces"
+                        >Designing interfaces that are <br />intuitive,
+                        efficient, and <br />enjoyable to use.</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="container-4e">
+                <div class="container-4f">
+                  <div class="svg-50"><div class="svg-51"></div></div>
+                </div>
+                <div class="container-52">
+                  <div class="container-53">
+                    <div class="container-54">
+                      <span class="web-mobile-app">Web & Mobile App</span>
+                    </div>
+                  </div>
+                  <div class="container-55">
+                    <div class="container-56">
+                      <span class="transforming-ideas"
+                        >Transforming ideas into <br />exceptional web and
+                        mobile <br />app experiences.</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="frame-57">
+              <div class="container-58">
+                <div class="container-59">
+                  <div class="svg-5a"><div class="svg-5b"></div></div>
+                </div>
+                <div class="container-5c">
+                  <div class="container-5d">
+                    <div class="container-5e">
+                      <span class="design-creative">Design & Creative</span>
+                    </div>
+                  </div>
+                  <div class="container-5f">
+                    <div class="container-60">
+                      <span class="crafting-visually-stunning"
+                        >Crafting visually stunning <br />designs that connect
+                        with your audience</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="container-61">
+                <div class="container-62">
+                  <div class="svg-63"><div class="svg-64"></div></div>
+                </div>
+                <div class="container-65">
+                  <div class="container-66">
+                    <div class="container-67">
+                      <span class="development">Development</span>
+                    </div>
+                  </div>
+                  <div class="container-68">
+                    <div class="container-69">
+                      <span class="bringing-vision-life"
+                        >Bringing your vision to life with <br />the latest
+                        technology and <br />design trends.</span
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <?php include "./footer/footer-desktop.php" ?>
+
+    </div>
+  </body>
+  <script src="./myjs/nav.js"></script>
+  <script src="myjs/eventhandling.js"></script>
+  
+</html>
